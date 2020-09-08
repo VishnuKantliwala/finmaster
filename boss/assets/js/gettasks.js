7ee150d8d5d83ed29834160676b9edb22e577aa7
Vishnu <vkantliwala@gmail.com>
2020-09-08 12:26:39 +0530

    small fixes and files added in task

--- boss/assets/js/gettasks.js
+++ boss/assets/js/gettasks.js
@@ -6,7 +6,6 @@ function viewTask(id) {
     
     $('.modal-body').empty();
     $(".taskLoader").show(500);
-    
     setTimeout(() => {
         $.ajax({
             url: "getTaskDetails.php",
@@ -47,6 +46,20 @@ function viewTask(id) {
                     
 
                     let row = "<h4>"+data[0].shipper_name+"</h4><p>"+data[0].date_assign+"</p><p>Quantity - "+data[0].task_emp_quantity+" | "+rd+"</p><h5>Description : </h5>" + data[0].task_emp_description;
+
+                    // row += '<div class="container">';
+                    row += '<h5>Files : </h5>';
+                    for(let i=0; i<data[0].task_file.length ; i++)
+                    {
+                        
+                        
+                        row += '<div class="col-md-4" style="padding:10px 0px">' +
+                        '<a target="_BLANK" href="./task_files/'+data[0].task_file[i]+'"> '+data[0].task_file[i]+' </a>'+
+                        "</div>";
+                        
+                    }
+                    
+                    // row += '</div>';
                     
                     $("#task_name").html(data[0].task_name);
                     
@@ -110,6 +123,17 @@ function viewRunningTask(id) {
                     
 
                     let row = "<h4>"+data[0].shipper_name+"</h4><p>"+data[0].date_assign+"</p><p>Quantity - "+data[0].task_emp_quantity+" | "+rd+"</p><h5>Description : </h5>";
+
+                    row += '<h5>Files : </h5>';
+                    for(let i=0; i<data[0].task_file.length ; i++)
+                    {
+                        
+                        
+                        row += '<div class="col-md-4" style="padding:10px 0px">' +
+                        '<a target="_BLANK" href="./task_files/'+data[0].task_file[i]+'"> '+data[0].task_file[i]+' </a>'+
+                        "</div>";
+                        
+                    }
                     
 
                     let rowQuantity = '';
