3ce81c0c6e647015088d06d1c9e63591b0b8a3d0
PREM PATEL <55750670+prem2697@users.noreply.github.com>
2020-09-17 17:58:49 +0530

    Update service.php

--- boss/service.php
+++ boss/service.php
@@ -136,7 +136,7 @@ if (isset($_POST['Submit'])) {
                                                         <input type="text" required name="txtGST" class="form-control" placeholder="%">
                                                     </div>
                                                 </div>
-                                                <div class="form-group row">
+<!--                                                 <div class="form-group row">
                                                     <label class="col-sm-2  col-form-label" for="txtCode">Renewal</label>
                                                     <div class="col-sm-10">
                                                         <input type="checkbox" name="chkRenew" data-plugin="switchery" data-color="#1AB394" data-secondary-color="#ED5565" data-size="small" />
@@ -147,7 +147,7 @@ if (isset($_POST['Submit'])) {
                                                     <div class="col-sm-10">
                                                         <input type="checkbox" name="chkTask" data-plugin="switchery" data-color="#1AB394" data-secondary-color="#ED5565" data-size="small" />
                                                     </div>
-                                                </div>
+                                                </div> -->
                                                 <div class="form-group row">
                                                     <label class="col-sm-2  col-form-label" for="example-placeholder"></label>
                                                     <div class="col-sm-10">
