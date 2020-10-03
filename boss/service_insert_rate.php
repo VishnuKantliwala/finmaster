9532b69e5f4cabb88dbd1b2c1aec53ed19a70379
PREM PATEL <55750670+prem2697@users.noreply.github.com>
2020-10-03 12:14:35 +0530

    Update service_insert_rate.php

--- boss/service_insert_rate.php
+++ boss/service_insert_rate.php
@@ -1,21 +1,9 @@
  <?php  
- 
+ session_start();
  include_once('../connect.php'); 
   include_once('commonFunc.php'); 
   include_once("image_lib_rname.php");
  
-	 
-	if (version_compare(phpversion(), '5.4.0', '<')) {
-	     if(session_id() == '') {
-		session_start();
-	     }
-	 }
-	 else
-	 {
-	    if (session_status() == PHP_SESSION_NONE) {
-		session_start();
-	    }
-	 }
 	$cn=new connect();
 	$cn->connectdb();
 			
