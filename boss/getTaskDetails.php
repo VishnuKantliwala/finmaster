4846170abecc17907bfbbf3ee37c77fcd039f897
PREM PATEL <55750670+prem2697@users.noreply.github.com>
2020-10-05 20:34:03 +0530

    Update getTaskDetails.php

--- boss/getTaskDetails.php
+++ boss/getTaskDetails.php
@@ -28,7 +28,7 @@ if(mysqli_num_rows($sql) > 0)
         $returnObj[$i]["task_emp_quantity"]=$row['task_emp_quantity'];
         $returnObj[$i]["task_emp_quantity_done"]=$row['task_emp_quantity_done'];
         $returnObj[$i]["task_emp_repetition_duration"]=$row['task_emp_repetition_duration'];        
-        $returnObj[$i]["date_assign"]=$date = date("j F Y - h : m A",strtotime($row['date_assign']));
+        $returnObj[$i]["date_assign"]=$date = date("j F Y - h : m",strtotime($row['date_assign']));
 
         $j=0;
         $sqlQty = $cn->selectdb( "SELECT `task_emp_status`, task_emp_qty FROM `tbl_task_emp_qty` WHERE task_emp_id=".$task_emp_id);
